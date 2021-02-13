2f3c36a3d3411d224d8c01e685e2d48fbad63723
Steven Baty <baty_steven@yahoo.com>
2021-02-12 19:25:08 -0700

    created class Manager and test, failed test first and then passed test

---
+++ __test__/Manager.test.js
@@ -0,0 +1,14 @@
+const { test, expect } = require('@jest/globals');
+const Manager = require('../lib/Employee.js');
+
+test('creates a manager object', () => {
+    const manager = new Manager('Steve');
+    manager.employeeID = 1;
+    manager.emailAddress = 'steve@email.com';
+    manager.officeNumber = '123a';
+
+    expect(manager.name).toBe('Steve');
+    expect(manager.employeeID).toEqual(expect.any(Number));
+    expect(manager.emailAddress).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(String));
+});
