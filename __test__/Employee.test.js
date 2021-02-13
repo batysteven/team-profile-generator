45474ff7f847695d29fc5f3ffd5f85a50dcee520
Steven Baty <baty_steven@yahoo.com>
2021-02-12 21:26:54 -0700

    add test for functions in Employee, failed first then pass

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -10,3 +10,23 @@ test('creates a employee object', () => {
     expect(employee.employeeID).toEqual(expect.any(Number));
     expect(employee.emailAddress).toBe('steve@email.com');
 });
+
+test('get employee name', () => {
+    const employee = new Employee('Steve');
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name));
+});
+
+test('get employee id', () => {
+    const employee = new Employee('Steve');
+    employee.employeeID = 1;
+
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.employeeID.toString()));
+});
+
+test('get employee email address', () => {
+    const employee = new Employee('Steve');
+    employee.emailAddress = 'steve@email.com';
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.emailAddress));
+});
