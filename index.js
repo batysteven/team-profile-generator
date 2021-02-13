fab68c3c2f5fe555fe8a1bf301489b4e0e868324
Steven Baty <baty_steven@yahoo.com>
2021-02-13 02:46:01 -0700

    add promptManger() uses inquirer to have user input manager data and save to new const manager

--- index.js
+++ index.js
@@ -1,6 +1,78 @@
 const inquirer = require('inquirer');
 const Engineer = require('./lib/Engineer');
-const Manger = require('./lib/Manager');
+const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 
-class 
+class teamGenerator {
+
+    // first thing when app is initiated, prompts user for manager information
+    promptManager() {
+
+        // create const of manager
+        const manager = new Manager;
+
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Welcome to Team Generator, please enter your team managers name. (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your team managers name.');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'employeeID',
+                message: 'Please enter the team managers ID. (Required)',
+                validate: employeeIDInput => {
+                    if (employeeIDInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your team managers ID.');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'emailAddress',
+                message: 'Please enter the team managers email address. (Required)',
+                validate: emailAddressInput => {
+                    if (emailAddressInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your team managers email address.');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'Please enter the team managers office number. (Required)',
+                validate: officeNumberInput => {
+                    if (officeNumberInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your team managers office number.');
+                        return false;
+                    }
+                }
+            },
+        ])
+        .then(({ name, employeeID, emailAddress, officeNumber }) => {
+            manager.name = name;
+            manager.employeeID = employeeID;
+            manager.emailAddress = emailAddress;
+            manager.officeNumber = officeNumber;
+        });
+    }
+}
+
+new teamGenerator().promptManager();
+
