61977d60741110dad8ac904d0a708c6254f25821
Steven Baty <baty_steven@yahoo.com>
2021-02-13 04:46:18 -0700

    successfully passed teamArray into teamHTML-template.js

--- index.js
+++ index.js
@@ -95,7 +95,8 @@ class teamGenerator {
                 this.promptEngineer();
             }
             if (response.questionNext === 2) {
-                this.generateTeamIndex();
+                generateTeamHTML(teamArray);
+                return writeFile(teamData);
             }
         });
     }
@@ -238,10 +239,24 @@ class teamGenerator {
         });
     }
 
-    generateTeamIndex() {
-
-    }
+    writeToFile = fileContent => {
+        return new Promise((resolve, reject) => {
+            fs.writeFile('./dist/index.html', fileContent, err => {
+                // if there's an error, reject the Promise and send the error to the Promise's .catch() method
+                if (err) {
+                    reject(err);
+                    //return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+                    return;
+                }
+    
+                //if everything went well, resolve the Promise and send the successful data to the .then() method
+                resolve({
+                    ok: true,
+                    message: 'File created!'
+                });
+            });
+        });
+    };
 }
 
 new teamGenerator().promptManager();
-
---
+++ src/teamHTML-template.js
@@ -0,0 +1,3 @@
+module.exports = teamData => {
+    const teamArray = teamData;
+};
