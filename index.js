b153da167142852ea28e4e23dcf893f0a8d0b285
Steven Baty <baty_steven@yahoo.com>
2021-02-20 04:22:21 -0700

    deleted commited out failed code no longer need

--- index.js
+++ index.js
@@ -106,23 +106,6 @@ class teamGenerator {
                     .then((teamData) => {
                         return this.writeToFile(teamData);
                     });
-                    // generateTeamHTML(teamArray);
-                    // generateTeamHTML.then((teamData) =>{
-                    //     writeToFile(teamData);
-                    // });
-                // })
-                // .then(pageHTML => {
-                //     return writeToFile(pageHTML);
-                // })
-                // .catch(err => {
-                //     console.log(err);
-                // });
-                // generateTeamHTML(teamArray);
-                // console.log(this);
-                // writeToFile();
-                // .catch(err => {
-                //     console.log(err);
-                // });
             }
         });
     }
@@ -272,32 +255,4 @@ class teamGenerator {
     }
 }
 
-// const writeToFile = fileContent => {
-//     return new Promise((resolve, reject) => {
-//         fs.writeFile('./dist/index.html', fileContent, err => {
-//             // if there's an error, reject the Promise and send the error to the Promise's .catch() method
-//             if (err) {
-//                 reject(err);
-//                 //return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
-//                 return;
-//             }
-
-//             //if everything went well, resolve the Promise and send the successful data to the .then() method
-//             resolve({
-//                 ok: true,
-//                 message: 'File created!'
-//             });
-//         });
-//     });
-// };
-
 new teamGenerator().promptManager();
-    // .then(teamArray => {
-    //     return generateTeamHTML(teamArray)
-    // })
-    // .then(pageHTML => {
-    //     return writeToFile(pageHTML);
-    // })
-    // .catch(err => {
-    //     console.log(err);
-    // });
--- src/teamHTML-template.js
+++ src/teamHTML-template.js
@@ -33,7 +33,6 @@ const generateTeamMember = memberData => {
             </section> 
             `;
         }
-        
     });
 }
 
