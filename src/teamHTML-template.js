f311fc5772009388450edaefa0d628193023d1e1
Steven Baty <baty_steven@yahoo.com>
2021-03-27 01:35:32 -0600

    add css

--- src/teamHTML-template.js
+++ src/teamHTML-template.js
@@ -1,44 +1,45 @@
 const generateTeamMember = memberData => {
-    console.log("member data");
-    console.log(memberData);
     const team = memberData.map(member => {
         if (member.getRole() === 'Manager') {
             return `
-            <section>
-            ${member.name}
-            ${member.getRole()}
-            ID: ${member.employeeID}
+            <section class="card">
+            <h3><b>${member.name}</b></h3>
+            <h4><p>${member.getRole()}</p></h4>
+            <div class="container">
+            <p>ID: ${member.employeeID}</p>
             <a href="mailto:${member.emailAddress}" class="name">Email: ${member.emailAddress}</a>
-            Office Number: ${member.officeNumber}
+            <p>Office Number: ${member.officeNumber}</p>
+            </div>
             </section> 
             `;
         }
         if (member.getRole() === 'Engineer') {
             return `
-            <section>
-            ${member.name}
-            ${member.getRole()}
-            ID: ${member.employeeID}
-            <a href="mailto:${member.emailAddress}">Email: ${member.emailAddress}</a>
-            <a href="${member.githubUsername}">GitHub: ${member.githubUsername}</a>
+            <section class="card">
+            <h3><b>${member.name}</b></h3>
+            <h4><p>${member.getRole()}</p></h4>
+            <div class="container">
+            <p>ID: ${member.employeeID}</p>
+            <a href="mailto:${member.emailAddress}" class="name">Email: ${member.emailAddress}</a>
+            <p><a href="${member.githubUsername}">GitHub: ${member.githubUsername}</a></p>
+            </div>
             </section> 
             `;
         }
         if (member.getRole() === 'Intern') {
             return `
-            <section>
-            ${member.name}
-            ${member.getRole()}
-            ID: ${member.employeeID}
-            <a href="mailto:${member.emailAddress}">Email: ${member.emailAddress}</a>
-            School: ${member.school}
+            <section class="card">
+            <h3><b>${member.name}</b></h3>
+            <h4><p>${member.getRole()}</p></h4>
+            <div class="container">
+            <p>ID: ${member.employeeID}</p>
+            <a href="mailto:${member.emailAddress}" class="name">Email: ${member.emailAddress}</a>
+            <p>School: ${member.school}</p>
+            </div>
             </section> 
             `;
         }
     });
-    console.log("after mapping");
-    console.log(team);
-    console.log(memberData);
     return team.join("");
 }
 
@@ -54,9 +55,12 @@ module.exports = teamData => {
         <title>Team Profile</title>
     </head>
     <style>
-        .name{color:red;}
+        .card{background-color: rgb(36, 219, 219); text-align: left; width: 200px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); border-radius: 5px; color: rgb(194, 98, 42); padding: 2px 16px; margin: 5px;}
+        .container{text-align: center; padding: 2px 16px; background-color: white; border-radius: 5px; margin-bottom: 10px;}
+        .header{background-color: green; color:white; text-align: center; padding-top: 20px; padding-bottom: 20px;}
     </style>
     <body>
+        <header class="header"><h1>My Team</h1></header>
         ${generateTeamMember(teamData)}
         
     </body>
